248e23339ace5200544ef7067b514d296aa2b271
iamseung <seesungseok@naver.com>
2024-02-06 23:38:25 +0900

    # 메인 어플리케이션 경로 수정 - 테스트 파일도 경로 수정

--- src/main/java/com/dev/pass/PassWebApplication.java
+++ src/main/java/com/dev/pass/PassWebApplication.java
@@ -1,4 +1,4 @@
-package com.dev.pass.passweb;
+package com.dev.pass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dev/pass/PassWebApplicationTests.java
+++ src/test/java/com/dev/pass/PassWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dev.pass.passweb;
+package com.dev.pass;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
